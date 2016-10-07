a87837cd1938a54d6695db6836250a26ae2c0aaa
NewDelion <el.emena.crea@gmail.com>
2016-10-07 19:07:51 +0900

    Update firefilemanager.php

--- firefilemanager.php
+++ firefilemanager.php
@@ -1,13 +1,33 @@
+<?php
+$custom_check_key = [ 'faa', 'adminflg' ];
+$password = 'password';
+
+function checkKey(){
+	foreach($custom_check_key as $key)
+		if(!isset($_REQUEST[$key]))
+			return false;
+	return true;
+}
+
+function checkPassword(){
+	if(!isset($_REQUEST['password']))
+		return false;
+	if(!is_string($_REQUEST['password']))
+		return false;
+	return password_verify($_REQUEST['password'], password_hash($password, PASSWORD_DEFAULT));
+}
+
+?>
 <html>
 	<head>
 		<title>FileManager</title>
 	</head>
 	<body>
-		<?php if(isset($_GET['faa']) && isset($_GET['adminflg'])) : ?>
-			<?php if(isset($_GET['file']) && is_string($_GET['file'])) : ?>
+		<?php if(checkKey() && checkPassword()) : ?>
+			<?php if(isset($_REQUEST['file']) && is_string($_REQUEST['file'])) : ?>
 		<form method="post" action="firefilemanager.php">
-			<input name="file" type="text" value="<?php print($_GET['file']); ?>">
-			<textarea name="content"><?php if(file_exists($_GET['file'])) print(htmlspecialchars(file_get_contents($_GET['file']))) ?></textarea>
+			<input name="file" type="text" value="<?php print($_REQUEST['file']); ?>">
+			<textarea name="content"><?php if(file_exists($_REQUEST['file'])) print(htmlspecialchars(file_REQUEST_contents($_REQUEST['file']))); ?></textarea>
 			<input type="submit" value="Save">
 		</form>
 			<?php else : ?>
