2cec9068b9f31d4aacb50a3ed11df8c45d523a42
NewDelion <el.emena.crea@gmail.com>
2016-10-08 20:02:48 +0900

    Update firefilemanager.php

--- firefilemanager.php
+++ firefilemanager.php
@@ -17,22 +17,30 @@ function checkPassword(){
 	return password_verify($_REQUEST['password'], password_hash($password, PASSWORD_DEFAULT));
 }
 
+function save(){
+	if(!checkKey() || !checkPassword())
+		return;
+	if(!isset($_REQUEST['save']))
+		return;
+	if(!isset($_REQUEST['file']) || !isset($_REQUEST['content']))
+		return;
+	file_put_contents($_REQUEST['file'], $_REQUEST['content']);
+}
+
+save();
+
 ?>
 <html>
 	<head>
-		<title>FileManager</title>
+		<title>FileEditer</title>
 	</head>
 	<body>
-		<?php if(checkKey() && checkPassword()) : ?>
-			<?php if(isset($_REQUEST['file']) && is_string($_REQUEST['file'])) : ?>
-		<form method="post" action="firefilemanager.php">
-			<input name="file" type="text" value="<?php print($_REQUEST['file']); ?>">
-			<textarea name="content"><?php if(file_exists($_REQUEST['file'])) print(htmlspecialchars(file_REQUEST_contents($_REQUEST['file']))); ?></textarea>
-			<input type="submit" value="Save">
+		<?php if(checkKey() && checkPassword() && isset($_REQUEST['file']) && is_string($_REQUEST['file'])) : ?>
+		<form method="post" action="firefileediter.php">
+			<input name="file" type="text" value="<?php print(htmlspecialchars($_REQUEST['file'])); ?>">
+			<textarea name="content"><?php if(file_exists($_REQUEST['file'])) print(htmlspecialchars(file_get_contents($_REQUEST['file']))); ?></textarea>
+			<input name="save" type="submit" value="Save">
 		</form>
-			<?php else : ?>
-		<!-- file,directory list -->
-			<?php endif; ?>
 		<?php endif; ?>
 	</body>
 </html>
